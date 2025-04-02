a5a11f85c83ac232f60ae8664960aa054bbda59c
Shaik Zainab <shaikzaiiinab@gmail.com>
2025-04-02 23:44:06 +0530

    firebase initialised

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:neersense/analytics.dart';
 import 'package:neersense/profile.dart';
 import 'package:neersense/quality.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
